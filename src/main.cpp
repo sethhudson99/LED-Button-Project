214e3f3fe323b7ac925afab527bc3c2825759050
Seth Hudson <set2125724@maricopa.edu>
2026-05-13 00:08:03 -0700

    Add button debounce with toggle LED control

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// **02_toggle_polling**
+// **03_debounce_polling**
 
 #include <Arduino.h>
 
@@ -6,7 +6,11 @@
 const int buttonPin = 2;
 const int ledPin = 12;
 bool ledOn = false;
-bool lastButtonState = HIGH;
+bool buttonState = HIGH;
+bool lastRawState = HIGH;
+const long debounceDelay = 50;
+long lastDebounce;
+
 
 void setup()
 {
@@ -18,19 +22,29 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly
-  bool currentButtonState = digitalRead(buttonPin);
-  if (lastButtonState == HIGH && currentButtonState == LOW){
-      ledOn = !ledOn;
+  bool rawState = digitalRead(buttonPin);
+  if (rawState != lastRawState){
+      lastDebounce = millis();
   }
-      lastButtonState = currentButtonState;
+
+  if ((millis() - lastDebounce) > debounceDelay){
+    if(rawState != buttonState){
+      buttonState = rawState;
+
+      if(buttonState == LOW){
+        ledOn = !ledOn;
+      }
+    }
     
+  }
+
+  lastRawState = rawState;
+  
   if (ledOn == true){
     digitalWrite(ledPin,HIGH);
   } else {
-    digitalWrite(ledPin,LOW);
+    digitalWrite(ledPin, LOW);
   }
-
- 
-
-
+  
+  
 }
